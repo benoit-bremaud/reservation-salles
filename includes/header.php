63e7248a119265aa11b2174890e64cdec130f8b2
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-19 16:40:42 +0200

    Create header.php
    Création du header.php avec les informations obligatoires HTML

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $titre ?? "Accueil" ?></title> <!-- titre dynamique en fonction de la page active-->
+    <!-- <link rel="stylesheet" href="../css/navbar_styles.css"> -->
+
+</head>
+<body>
